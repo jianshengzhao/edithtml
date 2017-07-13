0fd03e985ea184172054c09131ec937dcebd77a1
zjs <574712046@qq.com>
2017-07-13 16:07:05 +0800

    tag

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
           <i class="iconfont icon-weibiaoti1"></i>
           <span>量尺</span>
         </div>
-        <div class="tl_li">
+        <div class="tl_li tl_li_on gridli" @click="gridHangle">
           <i class="iconfont icon-xuxianxuankuang"></i>
           <span>格线</span>
         </div>
@@ -75,26 +75,157 @@
           </ul>
         </div>        
         <div class="tl_li" style="margin-left:50px;">
-          <i class="iconfont icon-baocun"></i>
+          <i class="iconfont icon-baocun2"></i>
           <span>保存</span>
         </div>
         <div class="tl_li">
           <i class="iconfont icon-yulan2"></i>
           <span>预览</span>
         </div>
-        <div class="tl_li">
+        <div class="tl_li outline">
           <i class="iconfont icon-tuichu"></i>
           <span>退出</span>
         </div>
-      </div>
+      </div> 
     </div>
     <!-- attribute tool -->
-    <div class="tool"></div>
+    <div class="tool">
+      <div class="toolBox">
+        <div class="property">
+          <label for="">x:</label>
+          <el-input v-model="inp_x" type='number' min='0' ></el-input>
+        </div>
+        <div class="property">
+          <label for="">y:</label>
+          <el-input v-model="inp_y" type='number' min='0' ></el-input>
+        </div>
+        <div class="property">
+          <label for="">宽:</label>
+          <el-input v-model="inp_w" type='number' min='0' ></el-input>
+        </div>
+        <div class="property">
+          <label for="">高:</label>
+          <el-input v-model="inp_h" type='number' min='0' ></el-input>
+        </div>
+        <div class="property">
+          <label for="">文字:</label>
+          <el-input v-model="inp_size" type='number' min='12' ></el-input>
+        </div>
+        <div class="property">
+          <label for="">字色:</label>
+          <el-color-picker v-model="color_font"></el-color-picker>
+        </div>
+        <div class="property">
+          <label for="">背景:</label>
+          <el-color-picker v-model="color_bg"></el-color-picker>
+        </div>
+      </div>
+    </div>
+    <!-- assembly library -->
+    <div class="library">
+      <div class="lib_box">
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>        
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+        <div class="lib_li" >模块</div>
+      </div>
+    </div>
+    <!-- editBox -->
+    <div class="editBox">
+      <div class="space">
+        <div class="canvas grid">
+          <div class="c_top"></div>
+          <div class="c_body"></div>
+          <div class="c_foot"></div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-  // import $ from 'jquery'
+  import $ from 'jquery'
   import '@/assets/iconfont/demo.css'
   import '@/assets/iconfont/iconfont.css'
   import Vue from 'vue'
@@ -108,10 +239,20 @@
         bgcoloer: false,
         color1: '#20a0ff',
         inp_width: 1200,
-        inp_height: 1600
+        inp_height: 1600,
+        inp_x: '',
+        inp_y: '',
+        inp_w: '',
+        inp_h: '',
+        inp_size: '',
+        color_font: '#333',
+        color_bg: '#fff'
       }
     },
-    created: function () {},
+    created: function () {
+      this.$nextTick(function () {
+      })
+    },
     methods: {
       bgColorShow: function () {
         var self = this
@@ -129,11 +270,22 @@
         var self = this
         if (self.inp_width < 980) {
           self.inp_width = 980
-          self.$notify({
-            title: '警告',
+          this.$message({
             message: 'pc页面宽度最好不要低于980',
             type: 'warning'
           })
+        }
+      },
+      gridHangle: function (e) {
+        let $gridli = $('.gridli')
+        let bloo = $gridli.hasClass('tl_li_on')
+        let canvas = $('.canvas')
+        if (bloo) {
+          $gridli.removeClass('tl_li_on')
+          canvas.removeClass('grid')
+        } else {
+          $gridli.addClass('tl_li_on')
+          canvas.addClass('grid')
         }
       }
     }
@@ -143,24 +295,67 @@
 <style>
 /*app*/
   #app {
+    position: relative;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
+    height: 100%;
   }
   *{
     margin:0;
     padding:0;
     font-family: -apple-system, "SF UI Text", "Helvetica Neue", Arial, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Zen Hei", sans-serif;
   }
-  input::-webkit-inner-spin-button{
-    display: none;
+  html,body{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  ::-webkit-scrollbar {
+      width: 4px;
+      height: 5px;
+  }
+  ::-webkit-scrollbar-track-piece
+    {
+        background-color: #fff;
+        -webkit-border-radius: 6px;
+    }
+    ::-webkit-scrollbar-thumb:vertical
+    {
+        height: 4px;
+        width: 3px;
+        background-color: #c9c9c9;
+        -webkit-border-radius: 3px;
+    }
+    ::-webkit-scrollbar-thumb:horizontal
+    {
+        width: 4px;
+        background-color: #c9c9c9;
+       /* -webkit-border-radius: 6px;*/
+    }
+
+  Pseudo ::scrollbar-corner element
+  ::-webkit-scrollbar-corner {
+      background-color: #c9c9c9;
+  }
+  Pseudo ::scrollbar-thumb element
+  ::-webkit-scrollbar-thumb {
+      background-color: #c9c9c9;
+      border-radius: 4px;
+  }
+  Pseudo ::scrollbar-track element
+  ::-webkit-scrollbar-track {
+      background-color: #c9c9c9;
   }
 /*top*/
   .top{
+    position: relative;
     height: 45px;
     min-width: 1024px;
-    border:1px solid #d9d9d9;
+    border-bottom: 1px solid #d9d9d9;
+    z-index: 4;
+    background-color: #fff;
   }
   .top>div{
     height: 45px;
@@ -177,13 +372,20 @@
     width: 300px;  
   }
   .tl_li{
+    padding: 2px;
+    border-radius: 2px;
     position: relative;
     display: inline-block;
-    width: 40px;
-    height: 37px;
-    margin: 4px;
+    width: 36px;
+    height: 34px;
+    margin: 2px;
     cursor: pointer; 
-    text-align: center;   
+    text-align: center; 
+    border:1px solid #fff;  
+  }
+  .tl_li_on{
+    background: rgba(0, 0, 0, 0.04);
+    border-color:rgba(0,0,0,.1);
   }
   .tl_li i{
     font-size: 18px;
@@ -222,6 +424,9 @@
   .toolbar li i{
     margin-right:5px;
   }  
+  .toolbar input::-webkit-inner-spin-button{
+    display: none;
+  }
   .t_right{
     position: relative;
     float: right;
@@ -239,9 +444,119 @@
   }
 /*tool*/
   .tool{
+    position: relative;
     min-width: 1024px;
     height: 34px;
-    border:1px solid #d9d9d9;
+    border-bottom: 1px solid #d9d9d9;
+    background-color: #fff;
+    z-index: 3;
   }
-
+  .toolBox{
+    margin: 0 auto;
+    width: 660px;
+    height: 34px;
+  }  
+  .property{
+    float: left;
+    width: 90px;
+    height: 34px;
+    line-height: 34px;
+    text-align: left;
+  }
+  .property .el-input{
+    display: inline-block;
+    width: 60px;    
+  }
+  .property input{
+    padding: 0;
+    height: 26px;
+    text-align: center;
+    text-indent: 5px;
+    border-radius: 0;
+    border:0;
+    border-bottom: 1px solid #d9d9d9;
+  }
+  .property .el-color-picker{
+    float: right;
+    margin-right:20px;
+  }
+  .property .el-color-picker__trigger{
+    border:0;
+  }
+  .property .el-color-picker__icon{
+    display: none;
+  }
+/*leftlibrary*/
+  .library{
+    position: absolute;
+    left: 0;
+    top: 0px;
+    padding-top:80px;
+    width: 181px;
+    height:100%;   
+    border-right: 1px solid #d9d9d9;
+    background-color: #fff;
+    box-sizing: border-box;
+    z-index: 2;
+  }
+  .lib_box{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    overflow-y:auto;
+  }
+  .lib_li{
+    float: left;
+    width: 58px;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;    
+  }
+  .lib_li:hover{
+    background-color: #f5f5f5;
+    cursor:pointer;
+  }
+/*editBox*/
+  .editBox{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 80px 0 0 181px;
+    box-sizing:border-box;    
+    background-color: #f5f5f5;    
+    z-index: 1;
+  }
+  .space{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow:auto;
+  }
+  .canvas{
+    position:absolute;
+    top: 100px;
+    left: 100px;
+    background-color: #fff;
+    width: 1200px;
+    height: 1800px;
+    margin-bottom: 100px;
+    background-size:10px 10px;
+    border:1px solid #d9d9d9;
+  } 
+  .grid{
+    background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUAQMAAAC3R49OAAAABlBMVEUAAAAnNk6AHRRIAAAAAnRSTlMAsyT7Lw4AAAANSURBVAjXY2hgoCoAACfQAIGM5uSyAAAAAElFTkSuQmCC);    
+  }
+  .c_top{
+    height: 200px;
+    border-bottom: 1px dashed #d9d9d9;
+  }
+  .c_body{
+    height: 1500px;
+    border-bottom: 1px dashed #d9d9d9;
+  }
+  .c_foot{
+    height: 100px;
+  }
 </style>
